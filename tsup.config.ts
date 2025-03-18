bc98f8b4eee10d7541daf1dc4258fef145afd221
Brendan Dash <me@aiwan.run>
2025-03-18 10:33:53 +0800

    chore: update

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,12 +4,6 @@ export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
   outDir: "dist",
-  outExtension: (ctx) => {
-    return {
-      js: ctx.format === "esm" ? ".mjs" : ".cjs",
-      dts: ".d.ts",
-    };
-  },
   sourcemap: true,
   dts: true,
   clean: true,
