666ebb4e98198e51c07262f1e45bdf54933a88be
Brendan Dash <me@aiwan.run>
2025-07-15 20:39:22 +0800

    feat: add ts map

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
-  outDir: 'dist',
   sourcemap: true,
-  dts: true,
-  clean: true,
+  dts: { sourcemap: true },
+  exports: true,
 })
